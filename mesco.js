0681269fca37e244d86c5f0b37659e52bc47edf3
FOXjustFOX <iglis.com20@gmail.com>
2025-01-12 19:35:40 +0100

    added the icon icon, fixed the problem with the rmv bg btn

--- mesco.js
+++ mesco.js
@@ -1,17 +1,30 @@
 const label = document.querySelector("#icon-image");
 const input = document.querySelector("#bg-image");
-const imageContainer = document.querySelector("#image-container");
 
 input.addEventListener("change", updateImageDisplay);
 
 function updateImageDisplay() {
-  const file = input.files[0];
-  if (validFileType(file)) {
-    imageContainer.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
-  }
+    const file = input.files[0];
+    if (validFileType(file)) {
+        imageContainer.style.backgroundImage = `url(${URL.createObjectURL(
+            file
+        )})`;
+        addImageIcon.style.display = "none";
+    }
 }
 
-const fileTypes = ["image/apng", "image/bmp", "image/gif", "image/jpeg", "image/pjpeg", "image/png", "image/svg+xml", "image/tiff", "image/webp", "image/x-icon"];
+const fileTypes = [
+    "image/apng",
+    "image/bmp",
+    "image/gif",
+    "image/jpeg",
+    "image/pjpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/tiff",
+    "image/webp",
+    "image/x-icon",
+];
 function validFileType(file) {
-  return fileTypes.includes(file.type);
+    return fileTypes.includes(file.type);
 }
